2423a82db622faa0eb5c044065a7ac30502a3780
thoby za <99469497+thobyza@users.noreply.github.com>
2022-11-22 22:24:00 +0700

    Add files via upload

---
+++ course.js
@@ -0,0 +1,18 @@
+/* Submit Button */
+function success(){
+  Swal.fire({
+    icon: 'success',
+    title: 'You have successfully logged in',
+    showConfirmButton: false,
+    timer: 1500
+  })
+}
+
+function success2(){
+  Swal.fire({
+    icon: 'success',
+    title: 'Your work has been saved',
+    showConfirmButton: false,
+    timer: 1500
+  })
+}
